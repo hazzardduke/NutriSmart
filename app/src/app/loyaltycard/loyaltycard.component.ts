731cfc32f47dff2eeb2bbe6d53c9e08078fc9c32
vargasaustin14 <avargas50946@ufide.ac.cr>
2025-04-01 13:54:23 -0600

    update

--- app/src/app/loyaltycard/loyaltycard.component.ts
+++ app/src/app/loyaltycard/loyaltycard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- IMPORTANTE
 
 @Component({
   selector: 'app-loyaltycard',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './loyaltycard.component.html',
   styleUrls: ['./loyaltycard.component.scss']
 })
-export class LoyaltyCardComponent {
-  llamitas = Array(12).fill(0);
-}
+export class LoyaltyCardComponent {}
